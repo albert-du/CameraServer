9aa9f89bb593bbb0964b1b6e8f57f25145851271
Albert Du <52804499+albert-du@users.noreply.github.com>
2023-02-07 17:36:49 -0800

    alignment and cropping

--- CameraServer.py
+++ CameraServer.py
@@ -4,6 +4,21 @@ import cv2
 
 app = Flask(__name__)
 
+height = 1080
+width = 1920
+
+cameras = dict()
+
+
+def getCamera(index):
+    cam = cameras.get(index)
+    if cam is None:
+        cam = VideoCapture(int(index))
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        cameras[index] = cam
+    return cam
+
 
 @app.route("/")
 def hello_world():
@@ -12,9 +27,30 @@ def hello_world():
 
 @app.route("/picture/<index>")
 def picture(index):
-    cam = VideoCapture(int(index))
+    cam = getCamera(index)
     result, image = cam.read()
     if result:
+        x = int((width - height) / 2)
+        image = image[0: height, x: x + height]
+        result, png = cv2.imencode(".png", image)
+        response = make_response(png.tobytes())
+        response.headers.set('Content-Type', 'image/png')
+        return response
+    else:
+        return "Could not take picture.", 400
+
+
+@app.route("/alignment/<index>")
+def alignment(index):
+    cam = getCamera(index)
+    result, image = cam.read()
+    if result:
+        x = int((width - height) / 2)
+        image = image[0: height, x: x + height]
+        cheight = image.shape[0]
+        cwidth = image.shape[1]
+        cv2.line(image, (0, 0), (cwidth, cheight), (127, 127, 127), 5)
+        cv2.line(image, (cwidth, 0), (0, cheight), (127, 127, 127), 5)
         result, png = cv2.imencode(".png", image)
         response = make_response(png.tobytes())
         response.headers.set('Content-Type', 'image/png')
